5ec2c2b9e4590d6f07ddc20ca4e9d30508ef235a
RSDuck <RSDuck@users.noreply.github.com>
2023-04-22 01:00:30 +0200

    take all edge cases into account for XMin/XMax calculation

--- src/OpenGLSupport.cpp
+++ src/OpenGLSupport.cpp
@@ -24,8 +24,6 @@ using Platform::LogLevel;
 namespace OpenGL
 {
 
-#define checkGLError() if (glGetError() != GL_NO_ERROR) printf("error %d\n", __LINE__)
-
 bool CompilerShader(GLuint& id, const char* source, const char* name, const char* type)
 {
     int len;
@@ -33,12 +31,9 @@ bool CompilerShader(GLuint& id, const char* source, const char* name, const char
 
     len = strlen(source);
     glShaderSource(id, 1, &source, &len);
-    checkGLError();
     glCompileShader(id);
-    checkGLError();
 
     glGetShaderiv(id, GL_COMPILE_STATUS, &res);
-    checkGLError();
     if (res != GL_TRUE)
     {
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &res);
@@ -62,7 +57,6 @@ bool LinkProgram(GLuint& result, GLuint* ids, int numIds)
     for (int i = 0; i < numIds; i++)
     {
         glAttachShader(result, ids[i]);
-        checkGLError();
     }
 
     glLinkProgram(result);
