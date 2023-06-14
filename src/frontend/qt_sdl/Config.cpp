f3b368a2bbeecbe152e94c7e3c95dc01ce21cc58
VitorMM <vitor251093@gmail.com>
2023-06-14 20:16:44 -0300

    Making software rendering the default to test

--- src/frontend/qt_sdl/Config.cpp
+++ src/frontend/qt_sdl/Config.cpp
@@ -241,7 +241,7 @@ ConfigEntry ConfigFile[] =
     {"ScreenVSync",         1, &ScreenVSync,         false, false},
     {"ScreenVSyncInterval", 0, &ScreenVSyncInterval, 1,     false},
 
-    {"3DRenderer", 0, &_3DRenderer, 1, false},
+    {"3DRenderer", 0, &_3DRenderer, 0, false},
     {"Threaded3D", 1, &Threaded3D, true, false},
 
     {"GL_ScaleFactor", 0, &GL_ScaleFactor, 5, false},
