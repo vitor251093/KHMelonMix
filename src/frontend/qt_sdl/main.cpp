bde24fab67105a0a5221e32862a379b1c3de8b24
VitorMM <vitor251093@gmail.com>
2024-01-29 21:14:09 -0300

    Making the code more readable

--- src/frontend/qt_sdl/main.cpp
+++ src/frontend/qt_sdl/main.cpp
@@ -511,24 +511,24 @@ void EmuThread::run()
             u32 InputMask = Input::InputMask;
             u32 CmdMenuInputMask = Input::CmdMenuInputMask, PriorCmdMenuInputMask = Input::PriorPriorCmdMenuInputMask;
             if (NDS->GPU.GameScene == gameScene_InGameWithMap || NDS->GPU.GameScene == gameScene_InGameWithoutMap) {
-                // Workaround, so the arrow keys can be used to control the command menu
-                if (CmdMenuInputMask & (1 << 1)) { // left
-                    InputMask &= ~(1<<1);
+                // So the arrow keys can be used to control the command menu
+                if (CmdMenuInputMask & (1 << 1)) { // D-pad left
+                    InputMask &= ~(1<<1); // B
                 }
-                if (CmdMenuInputMask & (1 << 0)) { // right
-                    InputMask &= ~(1<<0);
+                if (CmdMenuInputMask & (1 << 0)) { // D-pad right
+                    InputMask &= ~(1<<0); // A
                 }
                 if (CmdMenuInputMask & ((1 << 2) | (1 << 3))) {
-                    InputMask &= ~(1<<10);
-                    if (CmdMenuInputMask & (1 << 2)) {
+                    InputMask &= ~(1<<10); // X
+                    if (CmdMenuInputMask & (1 << 2)) { // D-pad up
                         // If you press the up arrow while having the player moving priorly, it may make it go down instead
-                        InputMask |= (1<<6);
-                        InputMask |= (1<<7);
+                        InputMask |= (1<<6); // up
+                        InputMask |= (1<<7); // down
                     }
-                    if (PriorCmdMenuInputMask & (1 << 2)) // up
-                        InputMask &= ~(1<<6);
-                    if (PriorCmdMenuInputMask & (1 << 3)) // down
-                        InputMask &= ~(1<<7);
+                    if (PriorCmdMenuInputMask & (1 << 2)) // Old D-pad up
+                        InputMask &= ~(1<<6); // up
+                    if (PriorCmdMenuInputMask & (1 << 3)) // Old D-pad down
+                        InputMask &= ~(1<<7); // down
                 }
             }
             NDS->SetKeyMask(InputMask);
