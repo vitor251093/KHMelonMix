095d4bd387b147ccbf75becea03d26152ea8514c
VitorMM <vitor251093@gmail.com>
2024-06-03 19:12:28 -0300

    ReCoded command menu should be adapted to every internal resolution

--- src/KHReCoded_GPU3D_OpenGL_shaders.h
+++ src/KHReCoded_GPU3D_OpenGL_shaders.h
@@ -31,6 +31,7 @@ void main()
     int zshift = (attr >> 16) & 0x1F;
 
     vec4 fpos;
+    float u3DScale = uScreenSize.x/256.0;
     fpos.xy = (((vec2(vPosition.xy) ) * 2.0) / uScreenSize) - 1.0;
     fpos.z = (float(vPosition.z << zshift) / 8388608.0) - 1.0;
     fpos.w = float(vPosition.w) / 65536.0f;
@@ -38,8 +39,8 @@ void main()
 
     float aspectRatio = TopScreenAspectRatio/(1.38);
     float commandMenuLeftMargin = 10.0*4;
-    float commandMenuWidth = 256.0*2;
-    float commandMenuHeight = 192.0*2;
+    float commandMenuWidth = (256.0*u3DScale)/4.0;
+    float commandMenuHeight = (192.0*u3DScale)/3.0;
     if (fpos.x >= -(1.00)*fpos.w && fpos.x <= -(0.375)*fpos.w &&
         fpos.y >= -(0.25)*fpos.w && fpos.y <= +(1.00)*fpos.w &&
         fpos.z == -1.0*fpos.w &&
