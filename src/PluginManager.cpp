1814d42d374830e2845f14d9fd562f01790b6c24
VitorMM <vitor251093@gmail.com>
2024-06-28 23:25:33 -0300

    Plugins now are in a list

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -8,16 +8,34 @@ namespace Plugins
 
 u32 PluginManager::GameCode = 0;
 
+struct IFactory { 
+    virtual bool isCart(int gameCode) = 0;
+    virtual Plugin* create(int gameCode) = 0;
+};
+
+template< typename Type > struct Factory : public IFactory {
+   virtual bool isCart(int gameCode) {
+      return Type::isCart(gameCode);
+   }
+   virtual Type* create(int gameCode) {
+      return new Type(gameCode);
+   }
+};
+
+IFactory* factories[] = {
+  new Factory<PluginKingdomHeartsDays>,
+  new Factory<PluginKingdomHeartsReCoded>
+};
+
 Plugin* PluginManager::load(u32 gameCode) {
     GameCode = gameCode;
     return get();
 }
 Plugin* PluginManager::get() {
-    if (PluginKingdomHeartsDays::isCart(GameCode)) {
-        return new PluginKingdomHeartsDays(GameCode);
-    }
-    if (PluginKingdomHeartsReCoded::isCart(GameCode)) {
-        return new PluginKingdomHeartsReCoded(GameCode);
+    for (IFactory* factory : factories) {
+        if (factory->isCart(GameCode)) {
+            return factory->create(GameCode);
+        }
     }
     return new PluginDefault();
 }
