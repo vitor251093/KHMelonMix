6fd55fdea16487ba1960970c4ee9d5a50f14063c
VitorMM <vitor251093@gmail.com>
2024-07-04 00:30:35 -0300

    Removing older launching method to avoid conflicts with melonDS

--- src/frontend/qt_sdl/Window.h
+++ src/frontend/qt_sdl/Window.h
@@ -117,7 +117,6 @@ public:
     void osdAddMessage(unsigned int color, const char* fmt, ...);
 
 protected:
-    void showEvent(QShowEvent* event) override;
     void resizeEvent(QResizeEvent* event) override;
     void changeEvent(QEvent* event) override;
 
@@ -228,8 +227,6 @@ private:
 
     int oldW, oldH;
     bool oldMax;
-
-    void loadMostRecentFile();
 
 public:
     ScreenPanel* panel;
