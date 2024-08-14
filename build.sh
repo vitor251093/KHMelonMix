71d36adbf8fd3ae093a2dc94c1ab9385596afec7
VitorMM <vitor251093@gmail.com>
2024-08-14 14:09:07 -0300

    Updating build script to make testing easier

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_QT6=ON -B build || exit 1
 cmake --build build -j$(nproc --all) || exit 1
 ./build/melonDS "roms/days.nds"
+# ./build/melonDS "roms/days_eu.nds"
+# ./build/melonDS "roms/days_jp.nds"
+# ./build/melonDS "roms/days_jp_rev1.nds"
