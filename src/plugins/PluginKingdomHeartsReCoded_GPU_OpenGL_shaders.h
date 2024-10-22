0faa9c4129bf2db0857220eaa5336a6a95b0cccb
VitorMM <vitor251093@gmail.com>
2024-10-21 23:21:46 -0300

    Fixing minor bug in ReCoded HUD

--- src/plugins/PluginKingdomHeartsReCoded_GPU_OpenGL_shaders.h
+++ src/plugins/PluginKingdomHeartsReCoded_GPU_OpenGL_shaders.h
@@ -467,7 +467,7 @@ vec2 getIngameHudTextureCoordinates(float xpos, float ypos)
 
         // next area name
         float sourceNextAreaNameHeight = 32.0;
-        float sourceNextAreaNameWidth = 90.0;
+        float sourceNextAreaNameWidth = 74.0;
         float nextAreaNameHeight = sourceNextAreaNameHeight;
         float nextAreaNameWidth = sourceNextAreaNameWidth*heightScale;
         float nextAreaNameBottomMargin = 0.0;
