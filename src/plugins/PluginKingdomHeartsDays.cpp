f285ed304ef6836128ee5603bf0b49e3f2ba8e1b
VitorMM <vitor251093@gmail.com>
2024-12-01 18:18:39 -0300

    Adding detection of camera coordinates (unoptimized)

--- src/plugins/PluginKingdomHeartsDays.cpp
+++ src/plugins/PluginKingdomHeartsDays.cpp
@@ -1509,12 +1509,50 @@ bool PluginKingdomHeartsDays::isSaveLoaded()
 
 void PluginKingdomHeartsDays::debugLogs(int gameScene)
 {
-    // nds->ARM7Write16(0x0225231c,   0x4000);
-    // nds->ARM7Write16(0x0225231c+2, 0x0000);
-    // nds->ARM7Write16(0x02252324,   0x4000);
-    // nds->ARM7Write16(0x02252324+2, 0x0000);
-    // nds->ARM7Write16(0x02236690,   0x8000);
-    // nds->ARM7Write16(0x02236690+2, 0x0000);
+    u32 pattern[5] = {0x000007fe, 0x00000ddc, 0x00001c72, 0x0000019a, 0x006a4000};
+    for (u32 addr = 0x02000000; addr < 0x02500000; addr+=4) {
+        u32 first = nds->ARM7Read32(addr);
+        if (first == pattern[0]) {
+            u32 second = nds->ARM7Read32(addr+4);
+            if (second == pattern[1]) {
+                u32 third = nds->ARM7Read32(addr+8);
+                if (third == pattern[2]) {
+                    u32 forth = nds->ARM7Read32(addr+12);
+                    if (forth == pattern[3]) {
+                        u32 fifth = nds->ARM7Read32(addr+16);
+                        if (fifth == pattern[4]) {
+                            printf("0x%08x\n", addr);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    printf("\n");
+
+    // 000007fe00000ddc00001c720000019a006a4000 0000a0750000150dffffaf330000cf1b00002f0ffffffa1b0000000000001000000000000000000300000004000000000000000000000000000000000000000000005c00000016b600001a00000014cd
+    // for (u32 addr = 0x0223229c - 0x20; addr <= 0x0223229c + 0x40; addr += 4) {
+    //     printf("%08x", nds->ARM7Read32(addr));
+    // }
+    // printf("\n");
+
+    // 000007fe00000ddc00001c720000019a006a4000 ffffe15e000014cd000019810000399d00002ecf000019810000000000001000000000001000000300000003000000000000000000000000000000000000000000005c00
+    // for (u32 addr = 0x022f3368 - 0x14; addr <= 0x022f3368 + 0x40; addr += 4) {
+    //     printf("%08x", nds->ARM7Read32(addr));
+    // }
+    // printf("\n");
+
+    // Roxas position coordinates in Agrabah
+    // PRINT_AS_32_BIT_HEX(0x021aa858);
+    // PRINT_AS_32_BIT_HEX(0x021aa85c);
+    // PRINT_AS_32_BIT_HEX(0x021aa860);
+
+    // nds->ARM7Write16(0x022f3370,   0x4000);
+    // nds->ARM7Write16(0x022f3370+2, 0x0000);
+    // nds->ARM7Write16(0x022f3368,   0x4000);
+    // nds->ARM7Write16(0x022f3368+2, 0x0000);
+    // nds->ARM7Write16(0x022f336c,   0x8000);
+    // nds->ARM7Write16(0x022f336c+2, 0x0000);
 
     // Twilight Town Lobby
     // PRINT_AS_32_BIT_HEX(0x022206c4); // direction (angle) where roxas is gonna move by going forward
@@ -1523,6 +1561,11 @@ void PluginKingdomHeartsDays::debugLogs(int gameScene)
     // PRINT_AS_32_BIT_HEX(0x022322a0); // camera coordinate inside the map (Z)
     // PRINT_AS_32_BIT_HEX(0x022322d4); // camera angle (X)
     // PRINT_AS_32_BIT_HEX(0x022322d8); // camera angle (Y)
+
+    // Agrabah
+    // PRINT_AS_32_BIT_HEX(0x022f3370); // camera coordinate inside the map (X)
+    // PRINT_AS_32_BIT_HEX(0x022f3368); // camera coordinate inside the map (Y)
+    // PRINT_AS_32_BIT_HEX(0x022f336c); // camera coordinate inside the map (Z)
 
     // PRINT_AS_8_BIT_HEX(0x0204c184);
     // PRINT_AS_8_BIT_HEX(0x0204c185);
