eb95eb5c81dbcbca58d45f21b87b3bd91a45df76
VitorMM <vitor251093@gmail.com>
2024-12-26 17:01:58 -0300

    Adding support to transparency

--- src/GPU2D_Soft.cpp
+++ src/GPU2D_Soft.cpp
@@ -2055,7 +2055,7 @@ void SoftRenderer::DrawSprite_Normal(u32 num, u32 width, u32 height, s32 xpos, s
 
         for (; xoff < xend;)
         {
-            unsigned char* pixel = imageData + (ypos * width + (xpos % width)) * (channels);
+            unsigned char* pixel = imageData + (ypos * width + ((xpos - orig_xpos) % width)) * (channels);
             color = (pixel[0] >> 3) | ((pixel[1] >> 3) << 0x5) | ((pixel[2] >> 3) << 0xA);
             bool visible = pixel[3] == 0xFF;
             if (visible) color |= 0x8000;
@@ -2298,6 +2298,14 @@ void SoftRenderer::DrawSprite_Normal(u32 num, u32 width, u32 height, s32 xpos, s
         {
             u32 og_pixel = objLine[xpos];
 
+            u16 alpha = 255;
+
+            if (spritemode == 3 || (!window && ((attrib[0] & 0x2000) ? (CurUnit->DispCnt & 0x80000000) : true)))
+            {
+                u32 tmpAlpha = (attrib[2] & 0xF000) >> 12;
+                alpha = tmpAlpha == 0 ? 0 : ((tmpAlpha + 1) << 4) - 1;
+            }
+
             u16 color = 0;
             if (og_pixel & 0x8000) {
                 color = og_pixel & 0x7FFF;
@@ -2310,17 +2318,23 @@ void SoftRenderer::DrawSprite_Normal(u32 num, u32 width, u32 height, s32 xpos, s
                 u16* extpal = CurUnit->GetOBJExtPal();
                 color = extpal[og_pixel & 0xFFF];
             }
-            
+            if (spritemode == 3 && (color & 0x8000) == 0) {
+                alpha = 0;
+            }
+            if (og_pixel == 0x80000) {
+                alpha = 0;
+            }
+
             u8 r = ((color & 0x001F) << 1) << 2;
             u8 g = ((color & 0x03E0) >> 4) << 2;
             u8 b = ((color & 0x7C00) >> 9) << 2;
 
-            unsigned char* pixel = imageData + (y * width + (xpos % width)) * (channels);
+            unsigned char* pixel = imageData + (y * width + ((xpos - orig_xpos) % width)) * (channels);
             
             pixel[0] = r;
             pixel[1] = g;
             pixel[2] = b;
-            pixel[3] = (spritemode == 3 && (color & 0x8000) == 0) ? 0 : 255;
+            pixel[3] = alpha;
 
             xoff++;
             xpos++;
