ab4ae399b17308834e526cf64818968c9c57137c
VitorMM <vitor251093@gmail.com>
2026-08-24 18:13:46 -0300

    Minor clean up

--- src/plugins/PluginKingdomHeartsDays.h
+++ src/plugins/PluginKingdomHeartsDays.h
@@ -147,7 +147,7 @@ private:
     bool SwitchTargetPressOnHold = false;
 
     std::array<CutsceneEntry, 46> Cutscenes;
-    std::array<CutsceneEntry, 102> Dialogues;
+    std::array<CutsceneEntry, 103> Dialogues;
     u32 cutscenesAddressOffset = 0;
     u32 _lastUnknownInEngineCutsceneAddress = 0;
 
